973e66ada59ced01e5bff4823ceeabd85a5e5572
nat-dias <nattaliadiasqueiroz@gmail.com>
2023-01-06 20:59:31 -0300

    tela básica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { AppContainer, Button, InputContainer, InputMessage, InputName, Main, Screen } from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  state = {
+    messages: [],
+    userValue: '',
+    messageValue: '',
+  }
+
+  onChengeUserValue = (event) => {
+    this.setState({ userValue: event.target.value })
+  }
+
+  onChengeMessageValue = (event) => {
+    this.setState({ messageValue: event.target.value })
+  }
+  sendMessage = () => {
+    const newMessage = {
+      user: this.state.userValue,
+      text: this.state.messageValue
+    }
+    console.log(newMessage)
+    const newMessagesArray = [newMessage, ...this.state.messages]
+    this.setState({messages: newMessagesArray, messageValue: ''})
+  }
+  render() {
+    return (
+      <AppContainer >
+          <Screen>
+            {this.state.messages.map((mes, index) => {
+              return (
+              <p key={index}>
+                <strong>{mes.user}</strong>:
+                <span> {mes.text}</span>
+              </p>)
+            })}
+          </Screen>
+
+          <InputContainer>
+            <InputName
+              placeholder='Nome'
+              onChange={this.onChengeUserValue}
+              value={this.state.userValue}
+            />
+
+            <InputMessage
+              placeholder='Mensagem'
+              onChange={this.onChengeMessageValue}
+              value={this.state.messageValue}
+            />
+
+            <Button
+              onClick={this.sendMessage}
+            >
+              Enviar
+            </Button>
+          </InputContainer>
+      </AppContainer>
+    );
+  }
 }
 
-export default App;
